b6cb787b249f5339fa265ce8a14c28514187a73c
unknown <williamjnrkdd@gmail.com>
2021-12-17 14:04:48 +0100

    webscraping and csv

--- web_scraping.py
+++ web_scraping.py
@@ -1,13 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import time
 import csv
 from bs4 import BeautifulSoup
 import pandas as pd
+
 pd.set_option('display.max_columns', None)
 
 browser = webdriver.Chrome('./chromedriver.exe')
 url = "https://www.nba.com/stats/players/traditional/?sort=PTS&dir=-1"
 browser.get(url)
+time.sleep(2)
+accept_button = browser.find_element_by_id("onetrust-accept-btn-handler")
+time.sleep(1)
+accept_button.click()
+time.sleep(2)
+browser.find_element_by_css_selector(".stats-table-pagination__select:nth-child(1) option").click()
 time.sleep(2)
 soup = BeautifulSoup(browser.page_source, "html.parser")
 headers = soup.select(".nba-stat-table__overflow table thead tr:nth-child(1)")
@@ -17,8 +25,8 @@ columns.extend([col.get_text(strip=True) for col in headers[0] if col.get_text(s
 
 # df = pd.DataFrame([], columns=columns)
 # for i in range(len(rows)):
-    # df.append([x.get_text(strip=True) for x in rows[i] if x.get_text(strip=True) != ""])
-    # print([x.get_text(strip=True) for x in rows[i] if x.get_text(strip=True) != ""])
+# df.append([x.get_text(strip=True) for x in rows[i] if x.get_text(strip=True) != ""])
+# print([x.get_text(strip=True) for x in rows[i] if x.get_text(strip=True) != ""])
 # print(df)
 rows = []
 for i in range(len(rows_soup)):
